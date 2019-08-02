f69e7ad94cafe8487bbd49ade12d2cb12f55865d
GB-Tocix <41319615+GB-Tocix@users.noreply.github.com>
2019-08-02 16:58:36 +0800

    八月第二天

--- src/__init__.py
+++ src/__init__.py
@@ -11,26 +11,56 @@ LEARNING_RATE_STEP = 100
 sess = tf.Session()
 
 
-def normalize_cols(m1):
+def normalize_cols_a(m0):
+    m1 = m0.copy()
+    for it in m1.index:
+        if -14.5 <= m1[it] <= -2.1:
+            m1[it] = (m1[it] + 14.5) / 12.4 / 16
+        elif -2.1 <= m1[it] <= -1.4:
+            m1[it] = (m1[it] + 2.1) / 0.7 / 16 + 0.0625
+        elif -1.4 <= m1[it] <= -0.7:
+            m1[it] = (m1[it] + 1.4) / 0.7 / 8 + 0.125
+        elif -0.7 <= m1[it] <= 0:
+            m1[it] = (m1[it] + 0.7) / 0.7 / 4 + 0.25
+        elif 0 <= m1[it] <= 0.7:
+            m1[it] = m1[it] / 0.7 / 4 + 0.5
+        elif 0.7 <= m1[it] <= 1.4:
+            m1[it] = (m1[it] - 0.7) / 0.7 / 8 + 0.75
+        elif 1.4 <= m1[it] <= 2.1:
+            m1[it] = (m1[it] - 1.4) / 0.7 / 16 + 0.875
+        elif 2.1 <= m1[it] <= 14.5:
+            m1[it] = (m1[it] - 2.1) / 12.4 / 16 + 0.9375
+
+    return m1
+
+
+def reverse_normalize_cols_a(m0):
+    m1 = m0.copy()
+    for it in range(len(m1)):
+        if 0 <= m1[it] <= 0.0625:
+            m1[it] = m1[it] * 12.4 * 16 - 14.5
+        elif 0.0625 <= m1[it] <= 0.125:
+            m1[it] = (m1[it] - 0.0625) * 0.7 * 16 - 2.1
+        elif 0.125 <= m1[it] <= 0.25:
+            m1[it] = (m1[it] - 0.125) * 0.7 * 8 - 1.4
+        elif 0.25 <= m1[it] <= 0.5:
+            m1[it] = (m1[it] - 0.25) * 0.7 * 4 - 0.7
+        elif 0.5 <= m1[it] <= 0.75:
+            m1[it] = (m1[it] - 0.5) * 0.7 * 4
+        elif 0.75 <= m1[it] <= 0.875:
+            m1[it] = (m1[it] - 0.75) * 0.7 * 8 + 0.7
+        elif 0.875 <= m1[it] <= 0.9375:
+            m1[it] = (m1[it] - 0.875) * 0.7 * 16 + 1.4
+        elif 0.09375 <= m1[it] <= 1:
+            m1[it] = (m1[it] - 0.9375) * 12.4 * 16 + 2.1
+
+    return m1
+
+
+def normalize_cols_b(m1):
     col_min = m1.min(axis=0)
     col_max = m1.max(axis=0)
-    # plt.plot(m1 / (col_max - col_min))
-    # plt.show()
     return (m1 - col_min) / (col_max - col_min)
-    # col_u = m1.mean(axis=0)
-    # col_std = m1.std(axis=0)
-    # plt.plot((m1 - col_u) / 2.414 / col_std)
-    # plt.show()
-    # return (m1 - col_u) / 4.414 / col_std
-
-
-def reverse_normalize_cols(m1, m2):
-    col_min = m1.min(axis=0)
-    col_max = m1.max(axis=0)
-    return m2 * (col_max - col_min) + col_min
-    # col_u = m1.mean(axis=0)
-    # col_std = m1.std(axis=0)
-    # return m2 * 4.414 * col_std + col_u
 
 
 def init_variable(shape):
@@ -57,10 +87,10 @@ test_end = n
 data_train = data.loc[train_start: train_end]
 data_test = data.loc[test_start: test_end]
 
-y_train = np.nan_to_num(normalize_cols(data_train.ix[:, 0]))
-y_test = np.nan_to_num(normalize_cols(data_test.ix[:, 0]))
-x_test = np.nan_to_num(normalize_cols(data_test.ix[:, 2:]))
-x_train = np.nan_to_num(normalize_cols(data_train.ix[:, 2:]))
+y_train = np.nan_to_num(normalize_cols_a(data_train.ix[:, 0]))
+y_test = np.nan_to_num(normalize_cols_a(data_test.ix[:, 0]))
+x_test = np.nan_to_num(normalize_cols_b(data_test.ix[:, 2:]))
+x_train = np.nan_to_num(normalize_cols_b(data_train.ix[:, 2:]))
 
 n_parameters = 31
 n_neurons_1 = 100
@@ -103,7 +133,7 @@ sess.run(init)
 loss_vec = []
 
 batch_size = 20
-step = 2000
+step = 500
 
 for i in range(step):
     rand_index = np.random.choice(np.arange(1, len(x_train)), size=batch_size)
@@ -124,9 +154,9 @@ plt.xlabel('Generation')
 plt.ylabel('Loss')
 plt.show()
 
-exactY_train = reverse_normalize_cols(data_train.ix[:, 0], np.transpose([y_train]))
+exactY_train = data_train.ix[:, 0]
 preY0_train = sess.run(out, feed_dict={X: x_train, Y: np.transpose([y_train])})
-preY1_train = reverse_normalize_cols(data_train.ix[:, 0], preY0_train)
+preY1_train = reverse_normalize_cols_a(preY0_train)
 
 plt.plot(exactY_train, 'g-', label="Exact", linewidth=0.2)
 plt.plot(preY1_train, 'r-', label="Prediction", linewidth=0.4)
@@ -135,9 +165,9 @@ plt.xlabel('Time')
 plt.ylabel('Rate_train')
 plt.show()
 
-exactY_test = reverse_normalize_cols(data_test.ix[:, 0], np.transpose([y_test]))
+exactY_test = data_test.ix[:, 0]
 preY0_test = sess.run(out, feed_dict={X: x_test, Y: np.transpose([y_test])})
-preY1_test = reverse_normalize_cols(data_test.ix[:, 0], preY0_test)
+preY1_test = reverse_normalize_cols_a(preY0_test)
 
 plt.plot(exactY_test, 'g-', label="Exact", linewidth=0.2)
 plt.plot(preY1_test, 'r-', label="Prediction", linewidth=0.4)
